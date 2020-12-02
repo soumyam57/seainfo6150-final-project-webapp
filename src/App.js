22fbfeb7e5118388128a8e0f2d22d7817a87e602
Soumya Murugendrappa <46505963+soumyam57@users.noreply.github.com>
2020-12-02 11:20:46 -0800

    deploy 5

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
     const fetchData = async () => {
       // put data fetching code here!
         const response = await fetch( 
-          "http://demo5934733.mockable.io/"
+          "https://demo5934733.mockable.io/"
         );
         const responseJson = await response.json();
           //console.log("Response",responseJson);
