1328a76bfb7db76c0196730ecf23feccc768131c
Soumya Murugendrappa <46505963+soumyam57@users.noreply.github.com>
2020-12-02 01:44:24 -0800

    Updates

---
+++ precache-manifest.06d46e76b2c8c9de0db654a326c5386a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425af7a0849418c65faf3bdd690c5e37",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "5e6d307bcb97f5222795",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.45b450ec.chunk.css"
+  },
+  {
+    "revision": "ab33d72cff42e0782c56",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.0bc773ff.chunk.js"
+  },
+  {
+    "revision": "5e6d307bcb97f5222795",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.96c688a9.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "502b5419ccc4d76d13fae9d3d8d3a3bf",
+    "url": "/seainfo6150-final-project-webapp/static/media/Birthday.502b5419.jpg"
+  },
+  {
+    "revision": "e9da3335d89dff786f646e4903a58f2b",
+    "url": "/seainfo6150-final-project-webapp/static/media/christmas.e9da3335.jpg"
+  },
+  {
+    "revision": "a3555f854579748a21859a10af49f751",
+    "url": "/seainfo6150-final-project-webapp/static/media/dolls.a3555f85.jpg"
+  },
+  {
+    "revision": "8141b3da382b8345c395edb82552558e",
+    "url": "/seainfo6150-final-project-webapp/static/media/getwell.8141b3da.jpg"
+  },
+  {
+    "revision": "16eaebeb4141ecb939b7667cdc72d781",
+    "url": "/seainfo6150-final-project-webapp/static/media/livetree.16eaebeb.jpg"
+  },
+  {
+    "revision": "099d501023cb185e30628301f365be38",
+    "url": "/seainfo6150-final-project-webapp/static/media/photoframe.099d5010.jpg"
+  },
+  {
+    "revision": "d3da4439c05de3e8d6ec916a1d770141",
+    "url": "/seainfo6150-final-project-webapp/static/media/sm.d3da4439.jpg"
+  },
+  {
+    "revision": "e97fc9bfb11b5dcd60a8a898cc0e3ce1",
+    "url": "/seainfo6150-final-project-webapp/static/media/wedding.e97fc9bf.jpg"
+  }
+]);
